229fb2bd9f9a76775d5f46b30150bd31a7f2e0e4
emon3455 <emon.mhk69@gmail.com>
2023-03-14 01:06:36 +0600

    display all and display few and other requirements done except modal

--- broke-phone/js/app.js
+++ broke-phone/js/app.js
@@ -1,4 +1,4 @@
-const loadPhones = async(searchText, dataLimit) =>{
+const loadPhones = async(searchText="iphone", dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
@@ -6,16 +6,19 @@ const loadPhones = async(searchText, dataLimit) =>{
 }
 
 const displayPhones = (phones, dataLimit) =>{
+
     const phonesContainer = document.getElementById('phones-container');
-    // phonesContainer.textContent = '';
+    phonesContainer.innerHTML="";
+
     // display 10 phones only 
     const showAll = document.getElementById('show-all');
-    if(dataLimit && phones.length > 10) {
+
+    if(dataLimit!= undefined && phones.length > 10) {
         phones = phones.slice(0, 10);
         showAll.classList.remove('d-none');
     }
     else{
-        showAll.classList.add('d-hidden');
+        showAll.classList.add('d-none');
     }
     
 
@@ -27,13 +30,17 @@ const displayPhones = (phones, dataLimit) =>{
     else{
         noPhone.classList.add('d-none');
     }
-    // display all phones
+
+
+ 
     phones.forEach(phone =>{
+
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
-        phonesContainer.innerHTML = `
+        
+        phoneDiv.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.images}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -44,6 +51,7 @@ const displayPhones = (phones, dataLimit) =>{
         `;
         phonesContainer.appendChild(phoneDiv);
     });
+
     // stop spinner or loader
     toggleSpinner(false);
 }
@@ -56,21 +64,21 @@ const processSearch = (dataLimit) =>{
 }
 
 // handle search button click
-document.getElementById('#btn-search').addEventListener('click', function(){
+document.getElementById('btn-search').addEventListener('click', function(){
     // start loader
     processSearch(10);
 })
 
 // search input field enter key handler
 document.getElementById('search-field').addEventListener('keypress', function (e) {
-    if (e.key === 'enter') {
+    if(e.key === 'Enter') {
         processSearch(10);
     }
 });
 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
-    if(!isLoading){
+    if(isLoading){
         loaderSection.classList.remove('d-none')
     }
     else{
@@ -83,6 +91,7 @@ const toggleSpinner = isLoading => {
 document.getElementById('btn-show-all').addEventListener('click', function(){
     processSearch();
 })
+
 
 const loadPhoneDetails = async id =>{
     const url =`www.openapi.programming-hero.com/api/phone/${id}`;
@@ -104,5 +113,3 @@ const displayPhoneDetails = phone =>{
         <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
     `
 }
-
-loadPhones('apple');
